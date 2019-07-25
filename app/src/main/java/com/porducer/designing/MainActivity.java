9265780dbf68070efb33b0210a111032c5bcfe8a
noareyenoar <noarlplp@gmail.com>
2019-07-25 22:08:22 +0700

    Going to implement screen slider

--- app/src/main/java/com/porducer/designing/MainActivity.java
+++ app/src/main/java/com/porducer/designing/MainActivity.java
@@ -1,14 +1,30 @@
 package com.porducer.designing;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner Mental_status,Pulse_strength;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Mental status spinner
+        Mental_status = findViewById(R.id.mental_spinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.metal_status, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Mental_status.setAdapter(adapter1);
+
+        //Pulse_strength spinner
+        Pulse_strength = findViewById(R.id.pulse_spinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.pulse_status, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Pulse_strength.setAdapter(adapter2);
     }
 }
