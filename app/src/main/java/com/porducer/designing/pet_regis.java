5025a247f3bd77b7491ce88ec8fc77767cfde8d8
Kitti <noar_lp@hotmail.com>
2019-08-04 15:54:22 +0700

    In Notebook : Going to add Pet_regis_Confirm

--- app/src/main/java/com/porducer/designing/pet_regis.java
+++ app/src/main/java/com/porducer/designing/pet_regis.java
@@ -26,6 +26,9 @@ public class pet_regis extends AppCompatActivity {
     DatePickerDialog datePickerDialog;
     Spinner species;
     EditText species_other,AnimalName,breed,color,OPD_HN;
+    Button OPD_Confirm;
+    Bundle data_from_petregis_packed;
+    TextView tvspecies,tvother,tvName,tvbirthday,tvsex,tvbreed,tvcolor,tvOPD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +38,22 @@ public class pet_regis extends AppCompatActivity {
         Birthdate = findViewById(R.id.petregis_textview_selectBirthday);
         sex_radiogroup = findViewById(R.id.petregis_radiogroup);
         species = findViewById(R.id.petregis_spinner_species);
+        OPD_Confirm = findViewById(R.id.petregis_confirm);
+        tvspecies = findViewById(R.id.petregis_textview_species);
+        tvother = findViewById(R.id.petregis_textview_speciesother);
+        tvName = findViewById(R.id.petregis_textview_name);
+        tvbirthday = findViewById(R.id.petregis_textview_Birthday);
+        tvsex = findViewById(R.id.petregis_textview_sex);
+        tvbreed = findViewById(R.id.petregis_textview_breed);
+        tvcolor = findViewById(R.id.petregis_textview_color);
+        tvOPD = findViewById(R.id.petregis_textview_OPD);
 
         ArrayAdapter<CharSequence> spinner_adapter_day = ArrayAdapter.createFromResource(pet_regis.this.getApplicationContext(),R.array.species, android.R.layout.simple_spinner_item);
         spinner_adapter_day.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         species.setAdapter(spinner_adapter_day);
 
         Birthdate.setOnClickListener(Birthdate_Clicked);
+        OPD_Confirm.setOnClickListener(OPD_Confirm_Clicked);
     }
 
     private View.OnClickListener Birthdate_Clicked = new View.OnClickListener() {
@@ -63,6 +76,14 @@ public class pet_regis extends AppCompatActivity {
             datePickerDialog.show();
         }
     };
+    private View.OnClickListener OPD_Confirm_Clicked = new View.OnClickListener() {
+        public void onClick(View v) {
+            if (data_validation()){
+                //data_packing();
+            }
+        }
+    };
+
     private String get_sex(){
         String selected_sex;
         int selectedRadioButtonID = sex_radiogroup.getCheckedRadioButtonId();
@@ -95,5 +116,15 @@ public class pet_regis extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Please Insert Species, Animal Name, OPD/HN Number",Toast.LENGTH_SHORT).show();
         }
         return answer;
+    }
+    private void data_packing(){
+        //TODO : Add Image upload system
+        data_from_petregis_packed.putString(tvbirthday.getText().toString(),Birthdate.getText().toString());
+        if (!breed.getText().toString().equals("")){data_from_petregis_packed.putString(tvbreed.getText().toString(),breed.getText().toString());}
+        /*data_from_petregis_packed.putString();
+        data_from_petregis_packed.putString();
+        data_from_petregis_packed.putString();
+        data_from_petregis_packed.putString();
+        data_from_petregis_packed.putString();*/
     }
 }
